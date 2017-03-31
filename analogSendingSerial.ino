2bb79565b44ad6448a4975516d22477fe0854f46
nobackes <ylan.rich@gmail.com>
2017-03-31 11:04:54 +0200

    Add files via upload

---
+++ analogSendingSerial.ino
@@ -0,0 +1,21 @@
+
+int port = A0;
+int val;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  pinMode(port, INPUT);
+    
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  analogRead(sensorPin);
+  Serial.println(val);
+
+  delay(15);
+}
